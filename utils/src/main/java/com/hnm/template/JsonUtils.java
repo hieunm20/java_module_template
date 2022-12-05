69667ec7688ab7711eb2068ef5f0f6d5a0ac0f53
hieunm <minhhieuittlu@gmail.com>
2022-12-05 10:28:59 +0700

    no message

--- utils/src/main/java/com/hnm/template/JsonUtils.java
+++ utils/src/main/java/com/hnm/template/JsonUtils.java
@@ -1,11 +1,14 @@
 package com.hnm.template;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
 /**
  * Created by Admin on 12/1/2022.
  */
 public class JsonUtils {
-    private static final ObjectMapper mapper = new ObjectMapper();
+    public static final Gson gson = new GsonBuilder().serializeNulls().create();
+    public static final ObjectMapper mapper = new ObjectMapper();
 
 }
